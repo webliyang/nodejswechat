1a9e298cbf20df9bce4103f5a165be615b700ddb
liyang <liyang3@17money.com>
2017-12-11 17:38:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var Koa = require('koa');
+var path = require('path')
+var wechat_file = path.join(__dirname,'/')
+var util = require('./libs/util.js')
+var wechat = require('./wechat/g.js')
+var config = {
+    wechat : {
+        appID:'wxd45c9f972c360b86',
+        appSecret:'361bbbc56d1c14f7767a22f2c45ed130',
+        token:'yangzaiyichenglocaltunnelme',
+        getAccessToken:function(){
+            return util.readFileAsync(wechat_file)
+        },
+        saveAccessToken:function(){
+            return util.writeFileAsync(wechat_file)
+        }
+    }
+}
+
+
+
+
+var app = new Koa();
+app.use(wechat(config.wechat))
+
+
+
+app.listen(3200);
+console.log('listening 3200')
---
+++ libs/util.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+var Promise = require('bluebird')
+module.exports = {
+    readFileAsync:function(fpath,encoding){
+        return new Promise(function(resolve,reject){
+            fs.readFile(fpath,encoding,function(err,content){
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(content)
+                }
+            })
+        })
+    },
+    writeFileAsync:function(fpath,content){
+        return new Promise(function(resolve,reject){
+            fs.writeFile(fpath,content,function(err){
+                if(err){
+                    reject(err)
+                }else{
+                    resolve()
+                }
+            })
+        })
+    }
+}
